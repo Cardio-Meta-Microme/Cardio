6a0564e5f40afe9b20305fb572c049d2d215934f
Mattias Tolhurst <tolhurstmattias@gmail.com>
2023-02-21 15:21:04 -0800

    Added interactivity

--- app.py
+++ app.py
@@ -19,15 +19,33 @@ df
 st.write(" # Adding some interactivity")
 
 def generate_table():
-    df = pd.DataFrame({
-    'first column': np.random.rand(5),
-    'second column': np.random.rand(5)
-    })
-
-    df
-
-if st.button(label="Generate DF"):
-    generate_table()
+    st.session_state['df'] = pd.DataFrame({
+        'first column': np.random.rand(5),
+        'second column': np.random.rand(5)
+        })
+    st.session_state['cols'] = 3
 
 
+col1, col2 = st.columns(2)
+
+if 'cols' not in st.session_state:
+    st.session_state['cols'] = 3
+
+if 'df' not in st.session_state:
+    st.session_state['df'] = pd.DataFrame({
+        'first column': np.random.rand(5),
+        'second column': np.random.rand(5)
+        })
+
+with col1:
+    if st.button(label="Clear DF"):
+        generate_table()
+
+with col2:
+    if st.button(label="Add Column"):
+        name = "column" + str(st.session_state.cols)
+        st.session_state.df[name] = np.random.rand(5)
+        st.session_state.cols += 1
+
+st.session_state.df
 
